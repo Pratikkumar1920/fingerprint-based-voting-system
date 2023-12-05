6084d3c21d6df151dcb4cc4d62c077bcc2fcd9af
Pratikkumar1920 <152950895+Pratikkumar1920@users.noreply.github.com>
2023-12-05 11:24:15 +0530

    Add files via upload

---
+++ onlinevoting/admin/inc/footer.php
@@ -0,0 +1,17 @@
+
+
+<div class= "row bg-dark  text-center text-white">
+
+        <div class= "col-12 my-3">
+            <p> &copy; copyright 2023- All Rights Reserved<br/>
+            Developer- Pratik & Suyash 
+
+            </p>
+        </div>
+</div>
+
+<script src ="../assets/js/jquery.min.js"></script>
+<script src ="../assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ onlinevoting/voters/inc/navigation.php
@@ -0,0 +1,20 @@
+<div class="row bg-red">
+    <div class="col-12 bg-red">
+    <nav class="navbar navbar-expand-lg navbar-light bg-red">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link text-white mr-3" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white mr-3" href="../admin/logout.php">Logout</a>
+        </li>
+         </ul>
+    </div>
+    </nav>
+</div>
+</div>
